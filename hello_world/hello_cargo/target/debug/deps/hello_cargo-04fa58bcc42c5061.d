499e2a5fc09e3c50b8fa373677aba9edd431f383
rabeebaqdas <rabeebaqdas@gmail.com>
2024-03-26 16:53:30 +0500

    chap 1 completed

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-04fa58bcc42c5061.d
@@ -0,0 +1,5 @@
+/home/rabeeb-aqdus-jilani/Desktop/Rabeeb/Rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-04fa58bcc42c5061: src/main.rs
+
+/home/rabeeb-aqdus-jilani/Desktop/Rabeeb/Rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-04fa58bcc42c5061.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/rabeeb-aqdus-jilani/Desktop/Rabeeb/Rust/hello_world/hello_cargo/target/debug/hello_cargo: /home/rabeeb-aqdus-jilani/Desktop/Rabeeb/Rust/hello_world/hello_cargo/src/main.rs
